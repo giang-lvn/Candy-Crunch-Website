19f0bd52057c292b7778f37776dc432d59c64f27
Giang Le <levungocgiang@gmail.com>
2025-12-01 00:35:29 +0700

    gogo

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,9 +2,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Array of hero line images for rotation
   const heroImages = [
-    '/asset/img/hero-line2.svg',
-    '/asset/img/hero-line3.svg',
-    '/asset/img/hero-line4.svg'
+    '/asset/img/hero-line2.webp',
+    '/asset/img/hero-line3.webp',
+    '/asset/img/hero-line4.webp'
   ];
   
   let currentImageIndex = 0;
