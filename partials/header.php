9b8e45814984f8425c94cd15635847fc0e23acc6
Ciara Nguyễn <giangnguyen.31231026898@st.ueh.edu.vn>
2025-12-21 02:20:28 +0700

    chỉnh lại ảnh chỗ header

--- partials/header.php
+++ partials/header.php
@@ -46,7 +46,7 @@ function is_active($page_name, $current_page_file) {
 <link rel="stylesheet" href="<?php echo $ROOT; ?>/views/website/css/main.css">
 
 <?php include 'cart.php'; ?>
-
+<?php include 'wishlist.php'; ?>
 
 <!-- HEADER NAV -->
 <div class="header-nav">
@@ -108,9 +108,9 @@ function is_active($page_name, $current_page_file) {
             </button>
 
             <!-- Wishlist -->
-            <a href="wishlist.php" class="action-item">
+            <button class="action-item" id="openWishlistBtn">
                 <img src="<?php echo $ROOT;?>/views/website/img/wishlist.svg" alt="Wishlist" class="action-icon"/>
-            </a>
+            </button>
 
             <!-- Account -->
             <a href="my_account.php" class="action-item">
@@ -145,15 +145,15 @@ function is_active($page_name, $current_page_file) {
                         <div class="menu-title">Hard Candy</div>
                         <div class="menu-items">
                             <a class="menu-item" href="product.php?id=milk-coffee-candy" 
-                            data-image="https://images.unsplash.com/photo-1575224300306-1b8da36134ec?w=400"
-                            data-title="Milk Coffee Candy"
+                            data-image="/views/website/img/milkcf.png"
+                            data-title="Milk-Filled Coffee Candy"
                             data-desc="Rich and creamy coffee-flavored hard candy with a smooth finish">
                                 Milk Coffee Candy
                             </a>
                             <a class="menu-item" href="product.php?id=fruit-candy"
-                            data-image="https://images.unsplash.com/photo-1582058091505-f87a2e55a40f?w=400"
+                            data-image="/views/website/img/fruitcandy.png"
                             data-title="Fruit Candy"
-                            data-desc="Assorted tropical fruit flavors bursting with natural sweetness">
+                            data-desc="Bright and juicy hard candies bursting with assorted fruit sweetness in every piece">
                                 Fruit Candy
                             </a>
                         </div>
@@ -164,13 +164,13 @@ function is_active($page_name, $current_page_file) {
                         <div class="menu-title">Filled-Hard Candy</div>
                         <div class="menu-items">
                             <a class="menu-item" href="product.php?id=caramel-coffee"
-                            data-image="https://images.unsplash.com/photo-1568471173238-64ed8e7e9815?w=400"
+                            data-image="/views/website/img/caramel.png"
                             data-title="Caramel-Filled Coffee Candy"
                             data-desc="Coffee candy with gooey caramel center for double indulgence">
                                 Caramel-Filled Coffee Candy
                             </a>
                             <a class="menu-item" href="product.php?id=milk-filled-coffee"
-                            data-image="https://images.unsplash.com/photo-1571091718767-18b5b1457add?w=400"
+                            data-image="/views/website/img/milk_filled.png"
                             data-title="Milk-filled Coffee Candy"
                             data-desc="Smooth milk filling wrapped in coffee-flavored shell">
                                 Milk-filled Coffee Candy
@@ -183,13 +183,13 @@ function is_active($page_name, $current_page_file) {
                         <div class="menu-title">Gummy</div>
                         <div class="menu-items">
                             <a class="menu-item" href="product.php?id=worm-gummies"
-                            data-image="https://images.unsplash.com/photo-1582058091505-be6f8b6c1c88?w=400"
+                            data-image="/views/website/img/worm.png"
                             data-title="Wiggly Worm Gummies"
                             data-desc="Fun worm-shaped gummies in fruity flavors kids love">
                                 Wiggly Worm Gummies
                             </a>
                             <a class="menu-item" href="product.php?id=bear-gummies"
-                            data-image="https://images.unsplash.com/photo-1625869016774-3a92be2ae2cd?w=400"
+                            data-image="/views/website/img/bear.png"
                             data-title="Tiny Bear Gummies"
                             data-desc="Adorable bear-shaped gummies packed with fruit flavors">
                                 Tiny Bear Gummies
@@ -202,7 +202,7 @@ function is_active($page_name, $current_page_file) {
                         <div class="menu-title">Chewing Gum</div>
                         <div class="menu-items">
                             <a class="menu-item" href="product.php?id=blueberry-chewy"
-                            data-image="https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=400"
+                            data-image="/views/website/img/blueberry.png"
                             data-title="Blueberry Crisp Chewy"
                             data-desc="Crispy shell with chewy center, sweet blueberry taste">
                                 Blueberry Crisp Chewy
@@ -214,13 +214,13 @@ function is_active($page_name, $current_page_file) {
                                 Mint Crisp Chewy
                             </a>
                             <a class="menu-item" href="product.php?id=cola-chewy"
-                            data-image="https://images.unsplash.com/photo-1629203851122-3726ecdf080e?w=400"
+                            data-image="/views/website/img/cola.png"
                             data-title="Cola Crisp Chewy"
                             data-desc="Classic cola taste in a fun chewing gum format">
                                 Cola Crisp Chewy
                             </a>
                             <a class="menu-item" href="product.php?id=strawberry-chewy"
-                            data-image="https://images.unsplash.com/photo-1588548961454-2b8e051686bf?w=400"
+                            data-image="/views/website/img/strawberry.png"
                             data-title="Strawberry Soft Chewy"
                             data-desc="Soft and sweet strawberry chewing gum">
                                 Strawberry Soft Chewy
@@ -233,25 +233,25 @@ function is_active($page_name, $current_page_file) {
                         <div class="menu-title">Marshmallow</div>
                         <div class="menu-items">
                             <a class="menu-item" href="product.php?id=vanilla-whirl"
-                            data-image="https://images.unsplash.com/photo-1606312619070-d48b4a0a4f06?w=400"
+                            data-image="/views/website/img/vanilla.png"
                             data-title="Vanilla Cotton Whirl"
                             data-desc="Cloud-like vanilla marshmallows that melt in your mouth">
                                 Vanilla Cotton Whirl
                             </a>
                             <a class="menu-item" href="product.php?id=chocolate-whirl"
-                            data-image="https://images.unsplash.com/photo-1612203985729-70726954388c?w=400"
+                            data-image="/views/website/img/chocolate.png"
                             data-title="Chocolate Cotton Whirl"
                             data-desc="Rich chocolate marshmallows with fluffy texture">
                                 Chocolate Cotton Whirl
                             </a>
                             <a class="menu-item" href="product.php?id=strawberry-whirl"
-                            data-image="https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=400"
+                            data-image="/views/website/img/strawberry_cotton.png"
                             data-title="Strawberry Cotton Whirl"
                             data-desc="Pink and fluffy strawberry marshmallow delights">
                                 Strawberry Cotton Whirl
                             </a>
                             <a class="menu-item" href="product.php?id=blueberry-cloud"
-                            data-image="https://images.unsplash.com/photo-1559156596-d0fdb07da244?w=400"
+                            data-image="/views/website/img/blueberry_fluffy"
                             data-title="Blueberry Fluffy Cloud"
                             data-desc="Light blueberry marshmallows with fruity burst">
                                 Blueberry Fluffy Cloud
@@ -264,13 +264,13 @@ function is_active($page_name, $current_page_file) {
                         <div class="menu-title">Collection</div>
                         <div class="menu-items">
                             <a class="menu-item" href="collection.php?id=tet"
-                            data-image="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?w=400"
+                            data-image="/views/website/img/dried_fruit.png"
                             data-title="Tet Collection"
                             data-desc="Special edition candy boxes for Lunar New Year celebrations">
                                 Tet Collection
                             </a>
                             <a class="menu-item" href="collection.php?id=christmas"
-                            data-image="https://images.unsplash.com/photo-1512909006721-3d6018887383?w=400"
+                            data-image="/views/website/img/winter_stick.png"
                             data-title="Christmas Collection"
                             data-desc="Festive candy assortments for holiday season">
                                 Christmas Collection
